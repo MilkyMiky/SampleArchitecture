9663986bbb491b57cbbf00e9cc4d3dfce20d8bef
Mikhail Lysyansky <mlysyansky@bekitzur.com>
2019-11-06 14:19:21 +0300

    Initial Commit

---
+++ SampleArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleArchitecture
+//
+//  Created by user on 06.11.2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
